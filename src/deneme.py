a79777999362537c07d5b75b26befee3c4fec835
jyri <ibrahim.cankurtran@ogrenci.selcuk.edu.tr>
2018-09-03 13:11:18 +0300

    update0.2

---
+++ src/deneme.py
@@ -0,0 +1,25 @@
+from keras.models import model_from_json
+from matplotlib.pyplot import plot as plt
+from keras.preprocessing import image
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot  as plt
+import numpy as np
+
+json_file = open('./model_num.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+loaded_model.load_weights("./model_num.h5")
+print("Loaded model from disk")
+
+loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+img = image.load_img(path="2.png",color_mode="grayscale",target_size=(28,28))
+img = image.img_to_array(img)
+X_train = img.reshape(1, 28, 28, 1)
+X_train = X_train.astype("float32")
+img_class = loaded_model.predict(X_train)
+print "Class : {} ".format(img_class)
--- src/kerasDigit.py
+++ src/kerasDigit.py
@@ -1,54 +1,64 @@
-from keras.datasets import mnist
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.preprocessing import image
-from keras.utils import to_categorical
+from keras.datasets import mnist # subroutines for fetching the MNIST dataset
+from keras.models import Model # basic class for specifying and training a neural network
+from keras.layers import Input, Dense, Flatten, Convolution2D, MaxPooling2D, Dropout
+from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+
+batch_size = 128 # in each iteration, we consider 128 training examples at once
+num_epochs = 5 # we iterate twelve times over the entire training set
+kernel_size = 3 # we will use 3x3 kernels throughout
+pool_size = 2 # we will use 2x2 pooling throughout
+conv_depth = 32 # use 32 kernels in both convolutional layers
+drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
+drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
+hidden_size = 128 # there will be 128 neurons in both hidden layers
+
+num_train = 60000 # there are 60000 training examples in MNIST
+num_test = 10000 # there are 10000 test examples in MNIST
+
+height, width, depth = 28, 28, 1 # MNIST images are 28x28 and greyscale
+num_classes = 10 # there are 10 classes (1 per digit)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data() # fetch MNIST data
+
+X_train = X_train.reshape(X_train.shape[0], height, width, depth)
+
+X_test = X_test.reshape(X_test.shape[0], height, width, depth)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255 # Normalise data to [0, 1] range
+X_test /= 255 # Normalise data to [0, 1] range
+
+Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
+Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+
+inp = Input(shape=(height, width, depth)) # N.B. TensorFlow back-end expects channel dimension last
+# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+conv_1 = Convolution2D(conv_depth, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+conv_2 = Convolution2D(conv_depth, (kernel_size, kernel_size), padding='same', activation='relu')(conv_1)
+pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+drop_1 = Dropout(drop_prob_1)(pool_1)
+flat = Flatten()(drop_1)
+hidden = Dense(hidden_size, activation='relu')(flat) # Hidden ReLU layer
+drop = Dropout(drop_prob_2)(hidden)
+out = Dense(num_classes, activation='softmax')(drop) # Output softmax layer
+
+model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
+
+model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+              optimizer='adam', # using the Adam optimiser
+              metrics=['accuracy']) # reporting the accuracy
+
+model.fit(X_train, Y_train, # Train the model using the training set...
+          batch_size=batch_size, epochs=num_epochs,
+          verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
+model.evaluate(X_test, Y_test, verbose=1) # Evaluate the trained model on the test set!
 
 
+from keras.models import model_from_json
+from keras.models import load_model
+model_json = model.to_json()
 
-def load_mnist_data():
+with open("model_num.json" , "w") as json_file:
+    json_file.write(model_json)
 
-    (train_images , train_labels) , (test_images , test_labels) = mnist.load_data()
-    return train_images , train_labels , test_images , test_labels
-
-
-
-trainImg , trainLab , testImg , testLab =load_mnist_data()
-
-def info_mnist_data(trImg=trainImg , trLab=trainLab , teImg= testImg , teLab = testLab):
-    classes = np.unique(trLab)
-    nClasses = len(classes)
-    print "Output Sayisi : {}  \nSiniflar : {}".format(nClasses , classes)
-
-    #sample figure on plot
-    print('Training data shape : ', trainImg.shape, trainLab.shape)
-    plt.figure(figsize=[10,5])
-    plt.subplot(121)
-    plt.imshow(trainImg[0 , : , : ] , cmap='gray')
-    plt.show(block= False)
-    raw_input("Press Enter to exit")
-    plt.close()
-
-info_mnist_data(trainImg , trainLab , testImg , testLab)
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-plt.figure(figsize=[10,5])
-plt.subplot(121)
-plt.imshow(train_images [ 0 , : , : ] , cmap= 'gray')
-plt.show()
-"""
-
-
-
-
+model.save_weights("model_num.h5")
